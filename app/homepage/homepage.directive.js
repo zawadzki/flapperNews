95c742210b1a9f35e1704a0a6f0a2bdc39257125
Damian Zawadzki <zawadsky@outlook.com>
2016-02-19 16:53:57 +0100

    Comments handler

--- app/homepage/homepage.directive.js
+++ app/homepage/homepage.directive.js
@@ -9,11 +9,66 @@
 	function posts() {
 		return {
 			posts: [
-				{title: 'Title One', link: 'http://google.pl', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.', upvotes: 5},
-				{title: 'Title Two', link: 'http://google.pl', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.', upvotes: 2},
-				{title: 'Title Three', link: 'http://google.pl', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.', upvotes: 15},
-				{title: 'Title Four', link: 'http://google.pl', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.', upvotes: 9},
-				{title: 'Title Five', link: 'http://google.pl', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.', upvotes: 4}
+				{
+                    title: 'Title One',
+                    link: 'http://google.pl',
+                    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.',
+                    upvotes: 10,
+                    comments: [
+                        { author: 'Joe', body: 'Cool post!', likes: 0 },
+                        { author: 'Bob', body: 'Great idea but everything is wrong!', likes: 0 }
+                    ]
+                },
+                {
+                    title: 'Title Two',
+                    link: 'http://google.pl',
+                    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.',
+                    upvotes: 9,
+                    comments: [
+                        { author: 'Joe', body: 'Cool post!', likes: 0 },
+                        { author: 'Bob', body: 'Great idea but everything is wrong!', likes: 0 }
+                    ]
+                },
+                {
+                    title: 'Title Three',
+                    link: 'http://google.pl',
+                    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.',
+                    upvotes: 8,
+                    comments: [
+                        { author: 'Joe', body: 'Cool post!', likes: 0 },
+                        { author: 'Bob', body: 'Great idea but everything is wrong!', likes: 0 }
+                    ]
+                },
+                {
+                    title: 'Title Four',
+                    link: 'http://google.pl',
+                    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.',
+                    upvotes: 7,
+                    comments: [
+                        { author: 'Joe', body: 'Cool post!', likes: 0 },
+                        { author: 'Bob', body: 'Great idea but everything is wrong!', likes: 0 }
+                    ]
+                },
+                {
+                    title: 'Title Five',
+                    link: 'http://google.pl',
+                    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.',
+                    upvotes: 6,
+                    comments: [
+                        { author: 'Joe', body: 'Cool post!', likes: 0 },
+                        { author: 'Bob', body: 'Great idea but everything is wrong!', likes: 0 }
+                    ]
+                },
+                {
+                    title: 'Title Six',
+                    link: 'http://google.pl',
+                    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias excepturi expedita facilis nesciunt rem rerum unde, velit veniam voluptas voluptates.',
+                    upvotes: 5,
+                    comments: [
+                        { author: 'Joe', body: 'Cool post!', likes: 0 },
+                        { author: 'Bob', body: 'Great idea but everything is wrong!', likes: 0 }
+                    ]
+                }
 			]
 		};
 	}
--- app/posts/posts.controller.js
+++ app/posts/posts.controller.js
@@ -9,10 +9,41 @@
 
 	function PostsCtrl($routeParams, posts) {
 		var vm = this;
-		vm.post = posts.posts[$routeParams.id];
 
-		console.log(vm);
-		console.log(posts);
+        vm.post = posts.posts[$routeParams.id];
+
+        //Adding comment handler
+        function Comment() {
+            this.body 	 = '';
+            this.likes = 0;
+        }
+
+        vm.newComment = new Comment();
+        vm.comments = posts.posts;
+        vm.addComment = addComment;
+
+        function addComment() {
+            vm.comments.push(vm.newComment);
+            vm.newComment = new Comment();
+        }
+
+        //Votes handler
+        vm.upVote = upVote;
+
+        function upVote(post) {
+            vm.post.upvotes ++;
+            vm.post.class = 'upvoted';
+        }
+
+        vm.downVote = downVote;
+
+        function downVote(post) {
+            vm.post.upvotes --;
+            vm.post.class = 'downvoted';
+        }
+
+        console.log(vm.post);
+        console.log($routeParams.id);
 	}
 
 })();
