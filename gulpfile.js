b993b11aa1e44de56a80dba9cc25d296e3c5bf2f
Damian Zawadzki <zawadsky@outlook.com>
2016-02-22 16:15:39 +0100

    Added few tasks to gulpfile #2

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+
+var del = require('del');
+var wiredep = require('wiredep');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var browserSync = require('browser-sync');
 
@@ -24,7 +27,9 @@ gulp.task('styles', function() {
                 autoprefixer: false,
                 discardComments: { removeAll: true }
             }),
-            autoprefixer({ browsers: ['last 2 versions'] })
+            autoprefixer({
+                browsers: ['last 2 versions']
+            })
         ]))
         .pipe(gulp.dest('.tmp/styles'))
         .pipe(reload({
@@ -32,12 +37,37 @@ gulp.task('styles', function() {
         }));
 });
 
+gulp.task('move', function(){
+    return gulp.src('./js/**/*.*', { cwd: 'app/' })
+        .pipe($.if('*.js', $.uglify()))
+        .pipe(gulp.dest('dist/js'));
+});
+
 gulp.task('fonts', function() {
-    return gulp.src(require('main-bower-files')({
-            filter: '**/*.{eot,svg,ttf,woff,woff2}'
-        }).concat('app/fonts/**/*'))
+    return gulp.src('./fonts/*.*', { cwd: 'app/' })
         .pipe(gulp.dest('.tmp/fonts'))
         .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('html', ['styles'], function() {
+    return gulp.src('app/*.html')
+        .pipe($.if('*.js', $.uglify()))
+        .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('wiredep', function() {
+    gulp.src('app/styles/*.scss')
+    .pipe(wiredep({
+        ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+
+gulp.src('app/*.html')
+    .pipe(wiredep({
+        ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
 });
 
 gulp.task('serve', ['styles', 'fonts'], function() {
@@ -60,4 +90,21 @@ gulp.task('serve', ['styles', 'fonts'], function() {
 
     gulp.watch('app/styles/**/*.scss', ['styles']);
     gulp.watch('app/fonts/**/*', ['fonts']);
+    gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+gulp.task('serve:dist', function() {
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: ['dist']
+            }
+        });
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('build', ['html', 'fonts', 'move'], function() {
+    return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
